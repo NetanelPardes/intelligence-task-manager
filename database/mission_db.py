5f4e1671239ccd89e2864eea1d3fbefb07d7eee8
Netanel Pardes <sendi8475@gmail.com>
2026-06-17 16:41:23 +0300

    Added a function to insert a new task, added a function to display the task by number

--- database/mission_db.py
+++ database/mission_db.py
@@ -1,16 +1,32 @@
 from db_connection import DBconnection
 import mysql.connector
 
-class missions:
+class MissionDB:
     def __init__(self, conn: DBconnection):
         self.connection = conn
     
     def create_mission(self,data):
-        pass
+        conn = self.connection.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("""INSERT INTO missions (title,description,location,difficulty,importance,risk_level) VALUES (%s,%s,%s,%s,%s,%s)""" ,
+                        (data['title'],data['description'],data['location'],data['difficulty'],data['importance'],(data['difficulty']*2)+data['importance']))
+        conn.commit()
+        new_id = cursor.lastrowid
+        conn.close()
+        cursor.close()
+        return self.get_mission_by_id(new_id)
+    
     def get_all_missions(self):
         pass
     def get_mission_by_id(self,id):
-        pass
+        conn = self.connection.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM missions WHERE id = %s" ,(id,))
+        mission = cursor.fetchone()
+        conn.close()
+        cursor.close()
+        return mission
+    
     def assign_mission(self,m_id, a_id):
         pass
     def update_mission_status(self,id, status):
@@ -27,3 +43,7 @@ class missions:
         pass
     def defget_top_agent(self,):
         pass
+if __name__ == "__main__":
+    connection = DBconnection()
+    mission = MissionDB(connection)
+    # print(mission.create_mission({"title":"dangerous","description": "Eliminate Khamenei","location":"Tehran, Iran","difficulty":9,"importance":7}))
