eb8e73d0efe3a1ef29bc66c8ced1c29cb71a4ad3
Netanel Pardes <sendi8475@gmail.com>
2026-06-17 16:28:28 +0300

    Start of class missions

--- database/mission_db.py
+++ database/mission_db.py
@@ -1,1 +1,29 @@
-   
+from db_connection import DBconnection
+import mysql.connector
+
+class missions:
+    def __init__(self, conn: DBconnection):
+        self.connection = conn
+    
+    def create_mission(self,data):
+        pass
+    def get_all_missions(self):
+        pass
+    def get_mission_by_id(self,id):
+        pass
+    def assign_mission(self,m_id, a_id):
+        pass
+    def update_mission_status(self,id, status):
+        pass
+    def get_open_missions_by_agent(self,id):
+        pass
+    def count_all_missions(self):
+        pass
+    def count_by_status(self,status):
+        pass
+    def count_open_missions(self,):
+        pass
+    def count_critical_missions(self,):
+        pass
+    def defget_top_agent(self,):
+        pass
