7a5f0926713be93f37ac352770c437e07b7117d7
Netanel Pardes <sendi8475@gmail.com>
2026-06-18 16:29:15 +0300

    Change adding a new task to clean code

--- routers/mission_routes.py
+++ routers/mission_routes.py
@@ -12,43 +12,37 @@ router = APIRouter()
 @router.post("/missions",status_code=201) 
 def add_new_mission(new_mission:dict):
     logger.info("A request to add a new task has been received.")
-    if  'title' in new_mission and new_mission['title']:
-        if  'description' in new_mission and new_mission['description']:
-            if 'location' in new_mission and new_mission['location']:
-                if 'difficulty' in new_mission and new_mission['difficulty']:
-                    if  'importance' in new_mission and new_mission['importance'] :
-                        if my_mission.check_importance_and_difficulty(new_mission['importance'] ,new_mission['difficulty']):
-                            risk = new_mission['difficulty'] * 2 + new_mission['importance']
-                            if 0 < risk < 10:
-                                risk_level = "LOW"
-                            elif risk < 18:
-                                risk_level = "MEDIUM" 
-                            elif risk < 25:
-                                risk_level = "HIGH"
-                            else:
-                                risk_level = "CRITICAL"
-                            new_mission['risk_level'] = risk_level
-                            logger.info("New task added successfully.")
-                            return {"data" : my_mission.create_mission(new_mission)}
-                        else:
-                            logger.error("Importance and difficulty should be between 1 and 10.")
-                            raise HTTPException(status_code=400,detail="Importance and difficulty should be between 1 and 10.")
-                    else:
-                        logger.error("The creation is missing a importance object.")
-                        raise HTTPException(status_code=422,detail="The creation is missing a importance object.")
-                else:
-                    logger.error("The creation is missing a difficulty object.")
-                    raise HTTPException(status_code=422,detail="The creation is missing a difficulty object.")
-            else:
-                logger.error("The creation is missing a location object.")
-                raise HTTPException(status_code=422,detail="The creation is missing a location object.")
-        else:
-            logger.error("The creation is missing a description object.")
-            raise HTTPException(status_code=422,detail="The creation is missing a description object.")
-    else:
+    if not 'title' in new_mission or not new_mission['title']:
         logger.error("The creation is missing a title object.")
         raise HTTPException(status_code=422,detail="The creation is missing a title object.")
-                        
+    if not 'description' in new_mission or not new_mission['description']:
+        logger.error("The creation is missing a description object.")
+        raise HTTPException(status_code=422,detail="The creation is missing a description object.")
+    if not 'location' in new_mission or not new_mission['location']:
+        logger.error("The creation is missing a location object.")
+        raise HTTPException(status_code=422,detail="The creation is missing a location object.")
+    if not 'difficulty' in new_mission or not new_mission['difficulty']:
+        logger.error("The creation is missing a difficulty object.")
+        raise HTTPException(status_code=422,detail="The creation is missing a difficulty object.")
+    if not 'importance' in new_mission or not new_mission['importance'] :
+        logger.error("The creation is missing a importance object.")
+        raise HTTPException(status_code=422,detail="The creation is missing a importance object.")
+    if not my_mission.check_importance_and_difficulty(new_mission['importance'] ,new_mission['difficulty']):
+        logger.error("Importance and difficulty should be between 1 and 10.")
+        raise HTTPException(status_code=400,detail="Importance and difficulty should be between 1 and 10.")
+    risk = new_mission['difficulty'] * 2 + new_mission['importance']
+    if 0 < risk < 10:
+        risk_level = "LOW"
+    elif risk < 18:
+        risk_level = "MEDIUM" 
+    elif risk < 25:
+        risk_level = "HIGH"
+    else:
+        risk_level = "CRITICAL"
+    new_mission['risk_level'] = risk_level
+    logger.info("New task added successfully.")
+    return {"data" : my_mission.create_mission(new_mission)}
+                             
 
 @router.get("/missions")
 def get_all_missions():
