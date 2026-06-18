b5ed6e8d752cf24a3d6d4faee5be3b84f7130f50
Netanel Pardes <sendi8475@gmail.com>
2026-06-18 11:28:44 +0300

    Adding a router to return agent by id and router toupdate agent

--- routers/agent_routes.py
+++ routers/agent_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,HTTPException
 from database.agent_db import AgentDB
 
-agent = AgentDB()
+my_agent = AgentDB()
 
 router = APIRouter()
 
@@ -9,20 +9,48 @@ router = APIRouter()
 
 @router.post("/agents",status_code=201)
 def add_new_agents(new_agent:dict):
-    if new_agent['agent_rank'] in ('Junior' , 'Senior' , 'Commander'):
-        if new_agent['name']:
-            if new_agent['agent_rank']:
-                if new_agent['specialty']:
-                    return {"message": agent.create_agent(new_agent)}
+    if new_agent['name'] and 'name' in new_agent:
+        if new_agent['agent_rank'] and 'agent_rank' in new_agent:
+            if new_agent['specialty']and 'specialty' in new_agent:
+                if new_agent['agent_rank'] in ('Junior' , 'Senior' , 'Commander'):
+                    return {"data": my_agent.create_agent(new_agent)}
                 else:
-                    raise HTTPException(status_code=422,detail="There is no specialty.")
+                    raise HTTPException(status_code=400,detail="Invalid agent rank.")
             else:
-                raise HTTPException(status_code=422,detail="There is no agent_rank.")
+                raise HTTPException(status_code=422,detail="There is no specialty.")
         else:
-            raise HTTPException(status_code=422,detail="There is no name.")
+            raise HTTPException(status_code=422,detail="There is no agent_rank.")
     else:
-        raise HTTPException(status_code=400,detail="Invalid agent rank.")
+        raise HTTPException(status_code=422,detail="There is no name.")
     
-@router.get("/agents")
+    
+@router.get("/agents",status_code=200)
 def all_agents():
-    return {"message":agent.get_all_agents()}
+    return {"data":my_agent.get_all_agents()}
+
+@router.get("/agents/{id}",status_code=200)
+def agent_by_id(id):
+        # if not isinstance(id,('int',)):
+        #     raise HTTPException(status_code=422, detail="That's not a number.")
+        agent = my_agent.get_agent_by_id(id)
+        if not agent:
+            raise HTTPException(status_code=404, detail="The agent does not exist in the system.")
+        return {"data":agent}
+        
+    
+@router.put("/agents/{id}")
+def update_agents(id,new_data:dict):
+#     if id != int:
+#         raise HTTPException(status_code=422, detail="That's not a number.")
+    agent = my_agent.get_agent_by_id(id)
+    if not agent:
+        raise HTTPException(status_code=404, detail="The agent does not exist in the system.")
+    if not new_data:
+        raise HTTPException(status_code=422 , detail="No data to update")
+    my_agent.update_agent(id,new_data)
+    
+
+
+# @router.put ("/agents/{id}/deactivate")
+
+# @router.get ("/agents/{id}/performance")
